139e0f5b702ec0312ad42ca663d7ed4b8b503b40
BenWyy <anhtu997788@gmail.com>
2026-04-01 22:25:02 +0700

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,7 +3,7 @@ import 'home_screen.dart';
 import 'register_screen.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key}); // ✅ FIX
+  const LoginScreen({super.key}); 
 
   @override
   Widget build(BuildContext context) {
